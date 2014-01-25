3d786f25cab9df3355ad8a5c1eaa14c041905331
Paulo Gaspar <anoyzz@gmail.com>
2014-01-25 02:10:39 +0000

    Starting project.

---
+++ svm.py
@@ -0,0 +1,26 @@
+
+
+class SVM:
+
+	def __init__(self, C=1.0, tol=1e-4, maxiter=10000, numpasses=10, kernel=linearKernel):
+
+		self.C = C
+		self.tol = tol
+		self.maxiter = maxiter
+		self.numpasses = numpasses
+		self.kernel = kernel
+
+
+	def train(self, data, class):
+
+		
+		pass
+
+
+
+
+
+	def linearKernel():
+
+		pass
+
